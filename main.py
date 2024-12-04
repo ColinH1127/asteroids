c4b777c30512613db6c169fc9a95d09cf3a75fcf
ColinH1127 <colinhussey82@gmail.com>
2024-12-04 12:15:47 -0500

    Used tick method so less resources will be used

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import pygame
 from constants import *
 
 def main():
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -10,9 +16,10 @@ def main():
                 return
         screen.fill((0, 0, 0), rect = (SCREEN_HEIGHT, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_WIDTH))
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
+    
 
 if __name__ == "__main__":
     main()          
