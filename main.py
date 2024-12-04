9609dc4bcda6ec94488e49551976ae8fd44d0615
ColinH1127 <colinhussey82@gmail.com>
2024-12-04 14:04:58 -0500

    completed movement functionality for player

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0, 0, 0), rect = (SCREEN_HEIGHT, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_WIDTH))
-        player.update(dt)
+        screen.fill((0, 0, 0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
         
+        
+if __name__ == "__main__":
+    main()         
     
 
-if __name__ == "__main__":
-    main()          
+         
